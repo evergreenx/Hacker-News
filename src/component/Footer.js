533bdbfe476a4c5b1d78e7d1fc70f667e1955c1e
ido evergreen <dicksonevergreenido@gmail.com>
2021-05-27 15:07:47 +0100

    added nav button

---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-primary mt-7 relative">
+        <div class="flex justify-center">
+        <img
+        alt="footer-logo"
+        src="https://res.cloudinary.com/evergreenx/image/upload/v1622122283/hackernews_1_1_zurvzk.svg"
+        className="my-13 w-6/12"
+      />
+        </div>
+    
+    </div>
+  );
+}
