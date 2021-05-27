62edc84a02975def430b5a71bbb870bad9e9e921
ido evergreen <dicksonevergreenido@gmail.com>
2021-05-27 13:58:11 +0100

    adding header

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-white flex justify-center mt-5 shadow-md mb-5">
+      <img
+        className="w-6/12 object-contain mb-5"
+        alt="logo"
+        src="https://res.cloudinary.com/evergreenx/image/upload/v1622120124/hackernews_1_w80fmi.svg"
+      />
+    </header>
+  );
+}
---
+++ src/component/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+
+export default function Home() {
+    return (
+
+        <>
+            <Header/>
+
+        <div className="container">
+            
+
+        </div>
+
+        </>
+    )
+}
